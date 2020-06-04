227d637ebc189958c7aa570c51a34c89b2415ba5
Vahe Poghosyan <vahepoghosyan97@gmail.com>
2020-06-04 15:23:18 +0400

    portfolio categories

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -11,9 +11,55 @@ export class PortfolioComponent implements OnInit {
 
   public faLightbulb = faLightbulb;
 
+  public categories = ['All', 'Buisness', 'Creative', 'Education', 'Blog'];
+  public selectedCategory = this.categories[0];
+  public images = [
+    {
+      image: 'assets/images/portfolio-1.png',
+      category: 'Buisness'
+    },
+    {
+      image: 'assets/images/portfolio-2.png',
+      category: 'Buisness'
+    },
+    {
+      image: 'assets/images/portfolio-3.png',
+      category: 'Creative'
+    },
+    {
+      image: 'assets/images/portfolio-4.png',
+      category: 'Education'
+    },
+    {
+      image: 'assets/images/portfolio-5.png',
+      category: 'Creative'
+    },
+    {
+      image: 'assets/images/portfolio-6.png',
+      category: 'Blog'
+    }
+  ];
+
+  public filteredPortfolio = [];
+
   constructor(public responsiveLayout: ResponsiveLayoutService) { }
 
   ngOnInit(): void {
+    this.filterPortfolio();
   }
 
+  filterPortfolio() {
+    if (this.selectedCategory === 'All') {
+      this.filteredPortfolio = this.images;
+    } else {
+      this.filteredPortfolio = this.images.filter(image => {
+        return this.selectedCategory === image.category;
+      });
+    }
+  }
+
+  changeCategory(category) {
+    this.selectedCategory = category;
+    this.filterPortfolio();
+  }
 }
